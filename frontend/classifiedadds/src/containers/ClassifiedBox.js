9c664fb6e8ec6bb9db62700c653776f1e3c65777
lacey <hiu@example.com>
2019-03-08 13:40:39 +0000

    change container box file

--- frontend/classifiedadds/src/containers/ClassifiedBox.js
+++ frontend/classifiedadds/src/containers/ClassifiedBox.js
@@ -2,13 +2,31 @@ import React, { Component } from 'react';
 import AdvertList from '../components/AdvertList'
 
 
-const ClassifiedBox = (props) => {
-    return(
+class ClassifiedBox extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            admins: []
+        }
+    }
+
+    componentDidMount(){
+        const url = "http://localhost:8080/admins"
+        fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data))
+    }
+
+
+    render(){
+        // console.log(this.state.admins)
+        return(
         <section>
             <h1>Hi front the ClassifiedBox</h1>
             <AdvertList/>
         </section>
     )
+        }  
 }
 
 export default ClassifiedBox;
