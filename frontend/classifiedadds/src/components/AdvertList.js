c234b3d2b55370588ea93cf1012e137bdeba0f2a
Joe McColl <joe4kool@gmail.com>
2019-03-08 14:33:11 +0000

    added key to advertList, bug fixed

--- frontend/classifiedadds/src/components/AdvertList.js
+++ frontend/classifiedadds/src/components/AdvertList.js
@@ -5,8 +5,8 @@ const AdvertList = ({adverts}) => {
     if (!adverts) return null;
     const advertNodes = adverts.map(advert => {
         return (
-            <div>
-            <h1 key = {advert.category}>{advert.title}</h1>
+            <div key = {advert.category}>
+            <h1>{advert.title}</h1>
             <p>{advert.category}</p>
             <p>{advert.askingPrice}</p>
             <p>{advert.description}</p>
