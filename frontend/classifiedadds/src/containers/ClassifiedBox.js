55e85e19670750a5bf8f8a4d12736fe4ae6714a7
Joe McColl <joe4kool@gmail.com>
2019-03-08 11:14:35 +0000

    added classifiedbox to render within app.jhs, working in browser

---
+++ frontend/classifiedadds/src/containers/ClassifiedBox.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+const ClassifiedBox = (props) => {
+    return(
+        <section>
+            <h1>Hi front the ClassifiedBox</h1>
+        </section>
+    )
+}
+
+
+
+export default ClassifiedBox;
