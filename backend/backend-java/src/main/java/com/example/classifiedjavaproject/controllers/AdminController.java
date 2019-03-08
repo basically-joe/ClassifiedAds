cdfa027c52837a6690a70ad2b406a750f5815e4c
lacey <hiu@example.com>
2019-03-08 13:31:04 +0000

    connect frontend to backend

--- backend/backend-java/src/main/java/com/example/classifiedjavaproject/controllers/AdminController.java
+++ backend/backend-java/src/main/java/com/example/classifiedjavaproject/controllers/AdminController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/administrators")
+
 public class AdminController {
 
     @Autowired
--- backend/backend-java/src/main/java/com/example/classifiedjavaproject/models/Advert.java
+++ backend/backend-java/src/main/java/com/example/classifiedjavaproject/models/Advert.java
@@ -6,9 +6,58 @@ public class Advert {
 
     private String category;
 
-    private String datePosted;
+    private String description;
 
-    
+    private String title;
+
+    private double askingPrice;
+
+    public Advert(String category, String description, String title, double askingPrice ){
+
+        this.category = category;
+        this.description = description;
+        this.title = title;
+        this.askingPrice = askingPrice;
+    }
 
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getAskingPrice() {
+        return askingPrice;
+    }
+
+    public void setAskingPrice(double askingPrice) {
+        this.askingPrice = askingPrice;
+    }
 }
