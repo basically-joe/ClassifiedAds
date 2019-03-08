222b8a69ca5ccbd3dc1cf136ec6689a978fa16ac
Joe McColl <joe4kool@gmail.com>
2019-03-08 11:24:07 +0000

    Advert list created and showing in browser

---
+++ frontend/classifiedadds/src/components/AdvertList.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+const AdvertList = (props) => {
+   return (
+       <section>
+           <h1>Hi front the  AdvertList</h1>
+       </section>
+   )
+
+
+
+}
+export default AdvertList;
--- frontend/classifiedadds/src/containers/ClassifiedBox.js
+++ frontend/classifiedadds/src/containers/ClassifiedBox.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import AdvertList from '../components/AdvertList'
 
 
 const ClassifiedBox = (props) => {
     return(
         <section>
             <h1>Hi front the ClassifiedBox</h1>
+            <AdvertList/>
         </section>
     )
 }
