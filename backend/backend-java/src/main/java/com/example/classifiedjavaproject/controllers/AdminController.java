3317b09ad901f50aa3d1112d00d08d979a843300
lacey <hiu@example.com>
2019-03-08 11:35:54 +0000

    add backend permission annotation

--- backend/backend-java/src/main/java/com/example/classifiedjavaproject/controllers/AdminController.java
+++ backend/backend-java/src/main/java/com/example/classifiedjavaproject/controllers/AdminController.java
@@ -3,17 +3,19 @@ package com.example.classifiedjavaproject.controllers;
 
 import com.example.classifiedjavaproject.repositories.AdminRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/administrators")
 public class AdminController {
 
     @Autowired
     AdminRepository adminRepository;
 
-
+  
 
 
 
