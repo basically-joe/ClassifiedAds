3238f92e21a8900f075d5ac16a064edf4bab4ac9
Joe McColl <joe4kool@gmail.com>
2019-03-09 19:12:00 +0000

    changed key on render of list, no key bug resolved

--- frontend/classifiedadds/src/components/AdvertList.js
+++ frontend/classifiedadds/src/components/AdvertList.js
@@ -7,7 +7,7 @@ const AdvertList = ({adverts}) => {
 
     const advertNodes = adverts.map(advert => {
         return (
-            <div key = {advert.category} className = "individual-ad-box-column">
+            <div key = {advert.title} className = "individual-ad-box-column">
             <h1>{advert.title}</h1>
             <p>{advert.category}</p>
             <p>{advert.askingPrice}</p>
