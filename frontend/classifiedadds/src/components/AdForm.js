7c99e9d50d775eca62c5025090534ac2c118ef8c
Joe McColl <joe4kool@gmail.com>
2019-03-09 19:49:44 +0000

    updated form to include radial buttons, render in browser

--- frontend/classifiedadds/src/components/AdForm.js
+++ frontend/classifiedadds/src/components/AdForm.js
@@ -1,78 +1,98 @@
-import React, {Component} from "react"
+import React, { Component } from "react"
 
 
-class AdForm extends Component{
+class AdForm extends Component {
 
     constructor(props) {
-    super(props)
-    this.state = {
-        askingPrice: 0,
-        category: "",
-        description: "",
-        title: ""
-    }
-    this.handleaskingPriceChange = this.handleaskingPriceChange.bind(this)
-    this.handleDescriptionChange = this.handleDescriptionChange.bind(this)
-    this.handleTitleChange = this.handleTitleChange.bind(this)
+        super(props)
+        this.state = {
+            askingPrice: "",
+            category: "",
+            description: "",
+            title: ""
+        }
+        this.handleaskingPriceChange = this.handleaskingPriceChange.bind(this)
+        this.handleDescriptionChange = this.handleDescriptionChange.bind(this)
+        this.handleTitleChange = this.handleTitleChange.bind(this)
+        this.handleCategory1Change = this.handleCategory1Change.bind(this)
 
-    this.handleSubmit = this.handleSubmit.bind(this)
-    
-    
-    }
-    
-    handleaskingPriceChange(e){
-        this.setState({askingPrice: e.target.value})
-    }
-    
-    handleDescriptionChange(e){
-        this.setState({description: e.target.value})
+
+        this.handleSubmit = this.handleSubmit.bind(this)
+
     }
 
-    handleTitleChange(e){
-        this.setState({title: e.target.value})
+    handleaskingPriceChange(e) {
+        this.setState({ askingPrice: e.target.value })
     }
-    
-    handleSubmit(e){
- 
+
+    handleDescriptionChange(e) {
+        this.setState({ description: e.target.value })
+    }
+
+    handleTitleChange(e) {
+        this.setState({ title: e.target.value })
+    }
+
+    handleCategory1Change(e) {
+        this.setState({ category: e.target.value })
+    }
+
+    handleSubmit(e) {
+
         e.preventDefault();
-        const newComment = {askingPrice: this.state.askingPrice, description: this.state.description, title: this.state.title}
+        const newComment = { askingPrice: this.state.askingPrice, category: this.state.category, description: this.state.description, title: this.state.title }
         this.props.onCommentSubmit(newComment)
-        this.setState({askingPrice: "", description: "", title: ""})
+        this.setState({ askingPrice: "", category: "", description: "", title: "" })
     }
-    
-        render(){
-            return(
-                <form 
+
+    render() {
+        return (
+            <form
                 className="ad-form"
                 onSubmit={this.handleSubmit}
-                >
-                
+            >
+
                 <input
-                type="text"
-                placeholder="Asking price eg £100"
-                value={this.state.askingPrice}
-                onChange={this.handleaskingPriceChange}
+                    type="text"
+                    placeholder="Asking price eg £100"
+                    value={this.state.askingPrice}
+                    onChange={this.handleaskingPriceChange}
                 />
                 <input
-                type = "text"
-                placeholder= "Description..."
-                value={this.state.description}
-                onChange={this.handleDescriptionChange}
+                    type="text"
+                    placeholder="Description..."
+                    value={this.state.description}
+                    onChange={this.handleDescriptionChange}
                 />
                 <input
-                type = "text"
-                placeholder= "Title..."
-                value={this.state.title}
-                onChange={this.handleTitleChange}
+                    type="text"
+                    placeholder="Title..."
+                    value={this.state.title}
+                    onChange={this.handleTitleChange}
                 />
+
+                <label>
+                    <input type="radio" onChange={this.handleCategory1Change.bind(this)}
+                        checked={this.state.category === "Beauty"}
+                        value="Beauty"
+                        name="radio1" />
+                    Beauty </label>
+
+                    <label>
+                    <input type="radio" onChange={this.handleCategory1Change.bind(this)}
+                        checked={this.state.category === "Automobile"}
+                        value="Automobile"
+                        name="radio2" />
+                    Automobile </label>
+
                 <input
-                type="submit"
-                value="Post"
+                    type="submit"
+                    value="Post"
                 />
-                </form>
-            )
-        }
+            </form>
+        )
     }
+}
 
 
 export default AdForm;
