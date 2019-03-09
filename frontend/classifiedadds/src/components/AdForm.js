4dce835b00fb4babda91d3323a659d0bd28dd49a
Joe McColl <joe4kool@gmail.com>
2019-03-09 14:42:49 +0000

    AdForm Route working in browser

---
+++ frontend/classifiedadds/src/components/AdForm.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const AdForm = (props) => {
+    return (
+        <div>
+        <h1>Hi from the AdForm class</h1>
+        </div>
+    )
+}
+
+export default AdForm;
--- frontend/classifiedadds/src/containers/ClassifiedBox.js
+++ frontend/classifiedadds/src/containers/ClassifiedBox.js
@@ -1,10 +1,12 @@
 import React, { Component, Fragment } from 'react';
 import AdvertList from '../components/AdvertList'
-import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import { BrowserRouter as Router, Route} from "react-router-dom";
 import Navbar from '../components/NavBar';
+import AdForm from "../components/AdForm"
+
 
 class ClassifiedBox extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
             admins: [],
@@ -12,35 +14,41 @@ class ClassifiedBox extends Component {
         }
     }
 
-    componentDidMount(){
+    componentDidMount() {
         const url = "http://localhost:8080/admins"
         const url2 = "http://localhost:8080/adverts"
 
         fetch(url)
-        .then(res => res.json())
-        .then(data => console.log(data))
-        
+            .then(res => res.json())
+            .then(data => console.log(data))
+
         fetch(url2)
-        .then(res => res.json())
-        .then(data => {
-            this.setState({advertsDB: data})
-            console.log(this.state.advertsDB._embedded);
-        })
+            .then(res => res.json())
+            .then(data => {
+                this.setState({ advertsDB: data })
+                console.log(this.state.advertsDB._embedded);
+            })
     }
 
-    render(){
-        return(
+    render() {
+        return (
 
             <Router>
                 <Fragment>
-                <Navbar/>
-        <section>
-            <AdvertList adverts = {this.state.advertsDB}/>
-        </section>
-        </Fragment>
-        </Router>
-    )
-        }  
+                    <Navbar />
+                       {/* <Route 
+                       exact path="/" 
+                       component={AdvertList}
+                       render= {() => <AdvertList prices = {this.state.advertsDB}/>}
+                       /> */}
+                       <Route path="/createad" component={AdForm}/>
+                    <section>
+                        <AdvertList adverts={this.state.advertsDB} />
+                    </section>
+                </Fragment>
+            </Router>
+        )
+    }
 }
 
 export default ClassifiedBox;
