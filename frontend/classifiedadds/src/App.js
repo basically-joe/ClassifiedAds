adf3897eca408d70e977edcce560aef4f4d47f11
Joe McColl <joe4kool@gmail.com>
2019-03-09 14:54:53 +0000

    made new css file for adlist, working in browser

--- frontend/classifiedadds/src/App.js
+++ frontend/classifiedadds/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ClassifiedBox from "../src/containers/ClassifiedBox"
 import './App.css';
+import "./Adlist.css";
 
 class App extends Component {
   render() {
