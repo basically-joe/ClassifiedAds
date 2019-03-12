105917332ab9d6b79474da375c0abccda541396e
Joe McColl <joe4kool@gmail.com>
2019-03-12 20:53:14 +0000

    change h3 to p tag for likes, working in browser

--- frontend/classifiedadds/src/components/LikesCounter.js
+++ frontend/classifiedadds/src/components/LikesCounter.js
@@ -4,7 +4,7 @@ const LikesCounter = () => {
     const [count, updateCount] = useState(0) // pass in an initial value
     return (
       <div>
-        <h3>Likes Counter: {count}</h3>
+        <p>Likes: {count}</p>
         <button onClick={() => updateCount(count + 1)}>Like </button>
         <button onClick={() => {if (count >0) {updateCount(count - 1)}}}>Dislike </button>
 
