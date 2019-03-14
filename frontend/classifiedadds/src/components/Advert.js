15d65760993344ede2c3682a0c01214d9bd7ab37
Joe McColl <joe4kool@gmail.com>
2019-03-14 21:19:12 +0000

    removed uneccessary css and moved likes counter

--- frontend/classifiedadds/src/components/Advert.js
+++ frontend/classifiedadds/src/components/Advert.js
@@ -23,13 +23,12 @@ if(adverts.length > advertsToShow.length && advertsToShow.length === 0){
 
         return (
             <div key = {advert.title} className = "individual-ad-box-column">
-            <h1>{advert.title}</h1>
+            <h1>{advert.title}</h1><LikesCounter/>
             <img alt="image_description" src={ (advert.image) }></img>
-            <p>Category: {advert.category}</p>
+            {/* <p>Category: {advert.category}</p>
             <p>Asking Price: £{advert.askingPrice}</p>
             <p>Description: {advert.description}</p>
-            <p>Posted By: {advert.adminRender ? getAdminById(admins, advert.adminRender) : ""}</p>
-            <LikesCounter/>
+            <p>Posted By: {advert.adminRender ? getAdminById(admins, advert.adminRender) : ""}</p> */}
             <button onClick = {()=> {handleAdvertToUpdate(advert)}}>Get ready to update</button>
             <UpdateModal  onClick = {()=> {handleAdvertToUpdate(advert) }} handleAdvertToUpdate={handleAdvertToUpdate} advertToUpdate={advertToUpdate} handleAdUpdate={handleAdUpdate}  triggerTextUpdate = {triggerTextUpdate} />
             <button onClick = {()=> {onAdDelete(advert.id)}}>Delete</button>
@@ -49,13 +48,12 @@ if(adverts.length > advertsToShow.length && advertsToShow.length === 0){
 
         return (
             <div key = {advert.title} className = "individual-ad-box-column">
-            <h1>{advert.title}</h1>
+            <h1>{advert.title}</h1><LikesCounter/>
             <img alt="image_description" src={ (advert.image) }></img>
-            <p>{advert.category}</p>
+            {/* <p>{advert.category}</p>
             <p>{advert.askingPrice}</p>
             <p>{advert.description}</p>
-            <p>{advert.admin ? advert.admin.userName : ""}</p>
-            <LikesCounter/>
+            <p>{advert.admin ? advert.admin.userName : ""}</p> */}
             <button onClick = {()=> {handleAdvertToUpdate(advert)}}>Get ready to update</button>
             <UpdateModal onClick = {()=> {handleAdvertToUpdate(advert) }} handleAdvertToUpdate={handleAdvertToUpdate} advertToUpdate={advertToUpdate} handleAdUpdate={handleAdUpdate}  triggerTextUpdate = {triggerTextUpdate} />
             <button onClick = {()=> {handleAdvertToUpdate(advert)}}>Update</button>
--- frontend/classifiedadds/src/components/LikesCounter.js
+++ frontend/classifiedadds/src/components/LikesCounter.js
@@ -4,13 +4,10 @@ const LikesCounter = () => {
     const [count, updateCount] = useState(0) // pass in an initial value
     return (
       <div>
-        <p>Likes: {count}</p>
-        <button onClick={() => updateCount(count + 1)}>Like </button>
-        {/* <button onClick={() => {if (count >0) {updateCount(count - 1)}}}>Dislike </button> */}
-
+        <p><button onClick={() => updateCount(count + 1)}>Like</button>  {count}</p>
+        
       </div>
     )
 }
-
 
 export default LikesCounter;
