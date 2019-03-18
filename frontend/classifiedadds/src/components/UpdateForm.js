ed0bf506fbc2d67cebb1a83e79b1384732f7a318
Joe McColl <joe4kool@gmail.com>
2019-03-18 17:32:32 +0000

    removed titles from update form as placeholders exist

--- frontend/classifiedadds/src/components/UpdateForm.js
+++ frontend/classifiedadds/src/components/UpdateForm.js
@@ -55,28 +55,24 @@ render() {
             <React.Fragment>
             
             <form className="content-update-form">
-                <label>Image:      </label>
             <input
                 type="text"
                 placeholder="Enter a URL"
                 value = {this.state.image}
                 onChange={this.handleImageChange}
             /><br/>
-                <label>Price(£):   </label>
                 <input
                     type="number"
-                    placeholder="Asking price..."
+                    placeholder="Asking price (£)..."
                     value={this.state.askingPrice}
                     onChange={this.handleaskingPriceChange}
                 /><br/>
-                <label>Description:</label>
                 <input
                     type="text"
                     placeholder="Description..."
                     value={this.state.description}
                     onChange={this.handleDescriptionChange}
                 /><br/>
-                <label>Title:      </label>
                 <input
                     type="text"
                     placeholder="Title..."
